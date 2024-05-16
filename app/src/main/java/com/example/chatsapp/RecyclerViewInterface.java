0d7a7d1b42224bbf311beb887be4e0391325fa0d
AmirSchreiber1 <amirschreiber1@gmail.com>
2024-05-16 11:00:12 +0300

    Remove files from repository

--- app/src/main/java/com/example/chatsapp/RecyclerViewInterface.java
+++
@@ -1,8 +0,0 @@
-package com.example.chatsapp;
-
-import com.example.chatsapp.adapters.ChatsListAdapter;
-
-public interface RecyclerViewInterface {
-    void onItemClick(int position);
-    void onItemLongClick(int position);
-}
